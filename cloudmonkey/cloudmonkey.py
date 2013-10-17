08148ebbde06e58ba6d9aa79892b28e780d73635
Chiradeep Vittal <chiradeep@apache.org>
2013-10-16 17:24:33 -0700

    Logout when exiting the CLI
    Signed-off-by: Chiradeep Vittal <chiradeep@apache.org>

--- cloudmonkey/cloudmonkey.py
+++ cloudmonkey/cloudmonkey.py
@@ -433,7 +433,7 @@ class CloudMonkeyShell(cmd.Cmd, object):
         Logout of session started with login with username and password
         """
         url = "%s://%s:%s%s" % (self.protocol, self.host, self.port, self.path)
-        logout(url, self.credentials['session'])
+        logout(url, self.credentials.get('session'))
         self.credentials['session'] = None
         self.credentials['sessionkey'] = None
 
@@ -508,6 +508,7 @@ class CloudMonkeyShell(cmd.Cmd, object):
         """
         Quit on Ctrl+d or EOF
         """
+        self.do_logout(None)
         sys.exit()
 
     def do_exit(self, args):
