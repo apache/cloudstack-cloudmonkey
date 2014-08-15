0b8e537e6723cd9922624968298a2c0688f1cfe6
Rohit Yadav <rohit.yadav@shapeblue.com>
2014-08-15 23:56:25 +0200

    config: remove user block, introduce server profiles
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- cloudmonkey/config.py
+++ cloudmonkey/config.py
@@ -42,7 +42,7 @@ config_dir = expanduser('~/.cloudmonkey')
 config_file = expanduser(config_dir + '/config')
 
 # cloudmonkey config fields
-config_fields = {'core': {}, 'server': {}, 'user': {}, 'ui': {}}
+config_fields = {'core': {}, 'ui': {}, 'server': {}}
 
 # core
 config_fields['core']['asyncblock'] = 'true'
@@ -50,6 +50,7 @@ config_fields['core']['paramcompletion'] = 'false'
 config_fields['core']['cache_file'] = expanduser(config_dir + '/cache')
 config_fields['core']['history_file'] = expanduser(config_dir + '/history')
 config_fields['core']['log_file'] = expanduser(config_dir + '/log')
+config_fields['core']['profile'] = 'server' # default server profile
 
 # ui
 config_fields['ui']['color'] = 'true'
