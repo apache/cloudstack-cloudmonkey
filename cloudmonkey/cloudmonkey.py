3d9d7795cdc9e3337a3493f2f370d801c4b4b96b
Phillip Kent <phillip.kent@xmlsoup.com>
2016-04-26 11:21:01 +0100

    fix bug in using readline in Windows

--- cloudmonkey/cloudmonkey.py
+++ cloudmonkey/cloudmonkey.py
@@ -67,7 +67,8 @@ except ImportError, e:
     print("Module readline not found, autocompletions will fail", e)
 else:
     import rlcompleter
-    if 'libedit' in readline.__doc__:
+    readline_doc = getattr(readline, '__doc__', '')
+    if readline_doc is not None and 'libedit' in readline_doc:
         readline.parse_and_bind("bind ^I rl_complete")
         readline.parse_and_bind("bind ^R em-inc-search-prev")
         normal_readline = False
