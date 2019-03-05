74ff37cffc1d8bb3652f6887faa770d933ffe768
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-03-05 17:26:48 +0530

    about: bump version to 6.0.0
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- config/about.go
+++ config/about.go
@@ -26,7 +26,7 @@ func (c *Config) Name() string {
 
 // Version CLI
 func (c *Config) Version() string {
-	return "6.0.0-beta3"
+	return "6.0.0"
 }
 
 // PrintHeader prints startup message in CLI mode
